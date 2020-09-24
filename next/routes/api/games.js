20595e31d0cb2a9ade50e87556c6c302ccda5849
mjaeschke <jaeschke21@gmail.com>
2020-09-24 18:41:19 -0500

    working on db stuff

--- next/routes/api/games.js
+++ next/routes/api/games.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const gamesController = require("../../controllers/gameControllers");
 
-// Matches with "/api/books"
+// Matches with "/api/games"
 router.route("/").get(gamesController.findAll).post(gamesController.create);
 
-// Matches with "/api/books/:id"
+// Matches with "/api/games/:id"
 router
   .route("/:id")
   .get(gamesController.findById)
